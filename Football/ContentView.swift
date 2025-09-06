46a8eac3ca4c347b1f04902562e8b3ba435c9c21
chen-domi <cchendominic@gmail.com>
2025-09-06 18:33:57 -0400

    Add text and images

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Football to You?")
+                .foregroundStyle(.green)
+                .font(.largeTitle)
+                .fontWeight(.thin)
         }
         .padding()
+        
+        HStack {
+            Image(systemName: "figure.american.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blue)
+            Image(systemName: "figure.australian.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.indigo)
+            Image(systemName: "figure.indoor.soccer")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.purple)
+        }
+        
     }
 }
 
